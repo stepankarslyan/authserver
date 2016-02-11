3230dc0f313672db5f36d31b9af8e6d76a229b03
Levon Hakopyan <Lev09@users.noreply.github.com>
2016-02-11 18:42:54 +0300

    Create Config.java

---
+++ src/main/java/hello/Config.java
@@ -0,0 +1,19 @@
+@Configuration
+public class Config {
+
+    @Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
+}
