b86a05aaff0d4ca44756c2ff3da60884f5985523
Stepan Gharslyan <stepan.karslyan@gmail.com>
2016-02-11 17:36:43 +0400

    Add user service

---
+++ src/main/java/hello/User.java
@@ -0,0 +1,48 @@
+package hello;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@javax.persistence.Entity
+@Table(name = "Users")
+public class User {
+	
+	@Id
+	@GeneratedValue(generator = "system-uuid")
+	@GenericGenerator(name = "system-uuid", strategy = "uuid")
+	@Column(name = "id")
+	@NotNull
+	private String id;
+	
+	@Column(name="login")
+	private String login;
+	
+	@Column(name="password")
+	private String passwrd;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPasswrd() {
+		return passwrd;
+	}
+	public void setPasswrd(String passwrd) {
+		this.passwrd = passwrd;
+	}
+	
+	
+}
---
+++ src/main/java/hello/UserController.java
@@ -0,0 +1,46 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RestController
+public class UserController {
+    
+	@Autowired
+	private UserRepository userRepository;
+	
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+    
+    @RequestMapping(value="/users", consumes = "application/json", method = RequestMethod.POST)
+    @ResponseBody
+    public void save(@RequestBody User user) {
+    	if(user!=null) {
+    		userRepository.save(user);
+    	}
+    	else {
+    		User newuser = new User();
+        	newuser.setLogin("Stepan");
+        	newuser.setPasswrd("password");
+        	userRepository.save(newuser);
+    	}	
+        
+    }
+    
+    @RequestMapping(value="/users", method = RequestMethod.GET)
+    @ResponseBody
+    public List<User> findAll() {	
+    	return userRepository.findAll();
+  
+    }
+    
+}
