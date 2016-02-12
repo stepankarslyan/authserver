e14269f32ad553d6f52a5d86649bb71137c985d8
Stepan Gharslyan <stepan.karslyan@gmail.com>
2016-02-12 13:03:14 +0400

    Add tthymeleaf template

--- src/main/java/hello/UserController.java
+++ src/main/java/hello/UserController.java
@@ -5,9 +5,11 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @RestController
@@ -16,9 +18,10 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 	
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+	@RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
     
     @RequestMapping(value="/users", method = RequestMethod.POST)
